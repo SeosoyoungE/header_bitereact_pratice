b6fe04be15175c80d4e54c96b369a08f461c6d58
CodeSandbox <support@codesandbox.io>
2023-11-28 05:58:23 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
@@ -7,32 +6,25 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>React + Vite</h1>
-      <h2>On CodeSandbox!</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR.
-        </p>
-
-        <p>
-          Tip: you can use the inspector button next to address bar to click on
-          components in the preview and open the code in the editor!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <div className="header__logo">
+          <a>LOGO</a>
+        </div>
+        <div className="header__nav">
+          <div className="header__nav__item header__nav__item-bold">
+            <a>HOME</a>
+          </div>
+          <div className="header__nav__item">
+            <a>STYLE</a>
+          </div>
+          <div className="header__nav__item">
+            <a>SHOP</a>
+          </div>
+          <div className="header__nav__item">
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
