5d8f8f5afe1e3b0fa9fc1714f7580ab46bc05351
soyoung <127925743+SeosoyoungE@users.noreply.github.com>
2023-12-07 07:33:51 +0000

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <header>
-        <div className="header__logo">
-          <a>LOGO</a>
-        </div>
-        <div className="header__nav">
-          <div className="header__nav__item header__nav__item-bold">
-            <a>HOME</a>
-          </div>
-          <div className="header__nav__item">
-            <a>STYLE</a>
-          </div>
-          <div className="header__nav__item">
-            <a>SHOP</a>
-          </div>
-          <div className="header__nav__item">
-            <i className="fa-solid fa-magnifying-glass"></i>
-          </div>
-        </div>
-      </header>
+      <Header />
     </div>
   );
 }
